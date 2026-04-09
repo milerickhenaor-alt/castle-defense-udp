0d7cd5db06d65554f7cb3e22e639e744bf0fe00a
SofiaRubioC-0317 <dianas.rubioc@autonoma.edu.co>
2026-04-09 08:12:13 -0500

    git commit -m "feat(controller): detectar input de teclado con pygame y generar acciones del jugador"

--- controller/input_handler.py
+++ controller/input_handler.py
@@ -1,18 +1,23 @@
 import pygame
 
-def procesar_input(jugadores):
-    acciones = []
+def process_input_local(player, controles):
+    """
+    player: El objeto Player del modelo.
+    controles: Diccionario con {up, down, shoot}.
+    """
+    keys = pygame.key.get_pressed()
+    accion = None
+    old_y = player.y
 
-    mouse_pos = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
+    # Movimiento Vertical
+    if keys[controles['up']] and player.y > 300:
+        player.y -= 5
+    if keys[controles['down']] and player.y < 550:
+        player.y += 5
 
-    # Solo click izquierdo
-    if click[0]:
-        for jugador in jugadores:
-            acciones.append({
-                "jugador_id": jugador.id,
-                "accion": "disparar",
-                "posicion": mouse_pos
-            })
-
-    return acciones
+    # Acción de Disparo
+    if keys[controles['shoot']]:
+        accion = "disparar"
+        
+    se_movio = (player.y != old_y)
+    return accion, se_movio
